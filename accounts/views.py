aeb981fe892597ac23f86befd2f68e0125e60561
magiulan <deico1232874@gmail.com>
2022-11-08 19:19:28 +0330

    add tests

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse_lazy
 
 from . forms import CustomUserCreationForm
 
+
 class SingUpView(generic.CreateView):
     form_class = CustomUserCreationForm
     template_name = 'registration/singup.html'
