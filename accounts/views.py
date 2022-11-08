4f6cd34cb401338b635df8d966cc6ac60fc7bd4b
magiulan <deico1232874@gmail.com>
2022-11-08 17:36:01 +0330

    create custom user with forms and signup

---
+++ accounts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
+
+from . forms import CustomUserCreationForm
+
+class SingUpView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'registration/singup.html'
+    success_url = reverse_lazy('login')
+
