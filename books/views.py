02bd587520ad8ca185d74d272fa77af032b615af
magiulan <deico1232874@gmail.com>
2022-11-09 16:23:57 +0330

    update view

--- books/views.py
+++ books/views.py
@@ -12,3 +12,15 @@ class BookListView(generic.ListView):
 class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'books/book_Detail.html'
+
+
+class BookCreateView(generic.CreateView):
+    model = Book
+    fields = ['title', 'author', 'description', 'price']
+    template_name = 'books/book_create.html'
+
+
+class BookUpdateView(generic.UpdateView):
+    model = Book
+    fields = ['title', 'author', 'description', 'price']
+    template_name = 'books/book_update.html'
